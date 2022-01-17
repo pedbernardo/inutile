785e7cd7f192395c587602bfa06631c84927dbfe
Pedro Bernardo <pedrovbernardo@gmail.com>
2022-01-17 16:47:06 -0300

    chore: add repo build and setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  ignorePatterns: ['dist']
+}
